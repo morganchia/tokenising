6a4d951127cdaeb5dd209f88b7dbcce5c15888ef
Morgan Chia <morganchia@gmail.com>
2026-05-28 23:56:10 +0800

    Changed ERC1155 to server side AES encryption

--- publish2heroku.bat
+++ publish2heroku.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Fixed issue with submitting and approving multiple contractors and payments"
+git commit -m "Changed ERC1155 to server side AES encryption"
 git push heroku master
 heroku logs --source app --tail
